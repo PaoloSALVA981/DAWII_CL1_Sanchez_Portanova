de22374eac366560ddc31cb1e87e7cf4bdaa40d3
PaoloSALVA981 <sanchezpaolo878@gmail.com>
2023-05-07 08:24:45 -0500

    Initial commit

---
+++ src/test/java/pe/edu/cibertec/DAWII_CL1_Sanchez_Portanova/DawiiCl1SanchezPortanovaApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAWII_CL1_Sanchez_Portanova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiiCl1SanchezPortanovaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
